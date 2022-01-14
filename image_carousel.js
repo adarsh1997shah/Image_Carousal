681300dfbc623c0070a467c0d9dee597bc46fa78
adarsh1997shah <adarshshah01@gmail.com>
2022-01-15 02:21:06 +0530

    code refactored and changed style

--- image_carousel.js
+++ image_carousel.js
@@ -1,107 +1,89 @@
-$( document ).ready( function() {
+//Globals
+const corausalInterval = 4000;
+let interval = null;
 
-	// For automatice carousal.
-	var interval = setInterval( slideInterval, 4000 );
-
-	function slideInterval() {
-
-		var item = $( '.active' );
-		var indicator_item = $( '.indicator-active' );
-		item.children().addClass( 'next' );
-
-		setTimeout( function() {
-
-			$( '.image-carousel-items-wrap' ).removeClass( 'active' );
-			$( '.image-carousel-indicator' ).removeClass( 'indicator-active' );
-			item.children().removeClass( 'next' );
-
-			if( item.next().length != 0 ) {
-
-				item.next().addClass( 'active' );
-				indicator_item.next().addClass( 'indicator-active' );
-			} else {
-
-				$( '.image-carousel-items-wrap:first-child' ).addClass( 'active' );
-				$( '.image-carousel-indicator:first-child' ).addClass( 'indicator-active' );
-			}
-		}, 600 );
+function resetInterval() {
+	// clearing time ineterval.
+	if (interval) {
+		clearInterval(interval);
 	}
 
+	// Starting time interval again.
+	interval = setInterval(
+		() => slide("next", true, "onClickNext"),
+		corausalInterval
+	);
+}
+
+function updateOnNext(item, indicator_item) {
+	if (item.next().length != 0) {
+		item.next().addClass("active");
+		indicator_item.next().addClass("indicator-active");
+	} else {
+		$(".image-carousel-items-wrap:first-child").addClass("active");
+		$(".image-carousel-indicator:first-child").addClass("indicator-active");
+	}
+}
+
+function updateOnPrev(item, indicator_item) {
+	if (item.prev().length != 0) {
+		item.prev().addClass("active");
+		indicator_item.prev().addClass("indicator-active");
+	} else {
+		$(".image-carousel-items-wrap:last-child").addClass("active");
+		$(".image-carousel-indicator:last-child").addClass("indicator-active");
+	}
+}
+
+function slide(className, isResetInterval, type) {
+	var item = $(".active");
+	var indicator_item = $(".indicator-active");
+	item.children().addClass(className);
+
+	if (isResetInterval) {
+		resetInterval();
+	}
+
+	setTimeout(function () {
+		$(".image-carousel-items-wrap").removeClass("active");
+		$(".image-carousel-indicator").removeClass("indicator-active");
+		item.children().removeClass(className);
+
+		if (type !== "onClickPrev") {
+			updateOnNext(item, indicator_item);
+		} else {
+			updateOnPrev(item, indicator_item);
+		}
+	}, 600);
+}
+
+$(document).ready(function () {
+	// For automatice carousal.
+	interval = setInterval(
+		() => slide("next", false, "auto"),
+		corausalInterval
+	);
+
 	// For previous click card.
-	$( '.image-carousel-control-prev' ).on( 'click', function() {
-
-		var item = $( '.active' );
-		var indicator_item = $( '.indicator-active' );
-		item.children().addClass( 'prev' );
-
-		// clearing time ineterval.
-		clearInterval( interval );
-
-		// Starting time interval again.
-		interval = setInterval( slideInterval, 4000 );
-
-		setTimeout( function() {
-
-			$( '.image-carousel-items-wrap' ).removeClass( 'active' );
-			item.children().removeClass( 'prev' );
-			$( '.image-carousel-indicator' ).removeClass( 'indicator-active' );
-			$( '.image-carousel-items-wrap' ).removeClass( 'active' );
-
-			$( this ).addClass( 'indicator-active' );
-
-			if( item.prev().length != 0 ) {
-
-				item.prev().addClass( 'active' );
-				indicator_item.prev().addClass( 'indicator-active' );
-			} else {
-
-				$( '.image-carousel-items-wrap:last-child' ).addClass( 'active' );
-				$( '.image-carousel-indicator:last-child' ).addClass( 'indicator-active' );
-			}
-		}, 600 );
-	} );
+	$(".image-carousel-control-prev").on("click", () =>
+		slide("prev", true, "onClickPrev")
+	);
 
 	// For next click card.
-	$( '.image-carousel-control-next' ).on( 'click', function() {
-
-		var item = $( '.active' );
-		var indicator_item = $( '.indicator-active' );
-		item.children().addClass( 'next' );
-
-		// clearing time ineterval.
-		clearInterval( interval );
-
-		// Starting time interval again.
-		interval = setInterval( slideInterval, 4000 );
-
-		setTimeout( function() {
-
-			$( '.image-carousel-items-wrap' ).removeClass( 'active' );
-			item.children().removeClass( 'next' );
-			$( '.image-carousel-indicator' ).removeClass( 'indicator-active' );
-			$( '.image-carousel-items-wrap' ).removeClass( 'active' );
-
-			if( item.next().length != 0 ) {
-
-				item.next().addClass( 'active' );
-				indicator_item.next().addClass( 'indicator-active' );
-			} else {
-
-				$( '.image-carousel-items-wrap:first-child' ).addClass( 'active' );
-				$( '.image-carousel-indicator:first-child' ).addClass( 'indicator-active' );
-			}
-		}, 600 );
-	} );
+	$(".image-carousel-control-next").on("click", () =>
+		slide("next", true, "onClickNext")
+	);
 
 	// For particular indicator click.
-	$( '.image-carousel-indicator' ).on( 'click', function() {
+	$(".image-carousel-indicator").on("click", function () {
+		var id = $(this).attr("id");
 
-		var id = $( this ).attr( 'id' );
+		resetInterval();
 
-		$( '.image-carousel-indicator' ).removeClass( 'indicator-active' );
-		$( '.image-carousel-items-wrap' ).removeClass( 'active' );
+		$(".image-carousel-indicator").removeClass("indicator-active");
+		$(".image-carousel-items-wrap").removeClass("active");
 
-		$( this ).addClass( 'indicator-active' );
-		$( `.items-wrap${id}` ).addClass( 'active' );
-	} );
-} );
+		$(this).addClass("indicator-active");
+		$(`.items-wrap${id}`).addClass("active");
+	});
+});
